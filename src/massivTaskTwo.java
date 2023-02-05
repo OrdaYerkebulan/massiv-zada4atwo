34ac04b5eafab1486923d14684bdd164291ae5fa
Orda Yerkebulan <ordaerkebulan@gmail.com>
2023-02-05 15:56:10 +0600

    massiv-taskTwo

---
+++ src/massivTaskTwo.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class massivTaskTwo {
+  public static void main(String[] args) {
+    int[] nums = {7, -3, 9, -11, 18, 99, 2, 11};
+    System.out.println("Исходный массив: " + Arrays.toString(nums));
+    System.out.print("Первая половина элементов:");
+    for (int i = 0; i < nums.length / 2; i++) {
+      System.out.print(" " + nums[i]);
+    }
+  }
+}
